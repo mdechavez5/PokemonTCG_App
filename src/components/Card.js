bed6e0d26fcfb57480bf1aa347a5939830b60b9c
mdechavez5 <marcdechavez@gmail.com>
2022-05-13 16:07:28 -0400

    Connected Card Component to PokemonTCG-API

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 
 function Card() {
+    const [card, setCard] = useState(null)
+    const URL = 'https://api.pokemontcg.io/v2/cards/xy1-1'
+
+    const getCard = () => {
+        fetch(URL)
+        .then(response => response.json())
+        .then(result => {
+            setCard(result.data)
+        })
+    } 
+
+    useEffect(() => getCard(),[])
+
+    console.log(card)
+
+    const display = () => {
+        return (
+            <div className='card'>
+                <h1>{card.name}</h1>
+                <p>{card.id}</p>
+                <img src={card.images.small} alt={card.name}/>
+                <p></p>
+            </div>
+            )
+    }
+
     return (
-        <div>
-            <h1>Card</h1>
-        </div>
+        !card ? <h1>Loading...</h1> : display()
     )
 }
 
