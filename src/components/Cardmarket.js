26eaad2ea62dfc7dec7966903912d8c85b7fd6fa
mdechavez5 <marcdechavez@gmail.com>
2022-05-16 10:40:39 -0400

    Added Footer Component

--- src/components/Cardmarket.js
+++ src/components/Cardmarket.js
@@ -11,27 +11,27 @@ function Cardmarket(props) {
                 <p>{props.cardmarket.updatedAt}</p>
                 <div className='column'>
                     <div className='row'>
-                        <p>Average Sell Price</p>
+                        <p>Average Sell Price: </p>
                         <p>{props.cardmarket.prices.averageSellPrice}</p>
                     </div>
                     <div className='row'>
-                        <p>Low Price</p>
+                        <p>Low Price: </p>
                         <p>{props.cardmarket.prices.lowPrice}</p>
                     </div>
                     <div className='row'>
-                        <p>trendPrice</p>
+                        <p>Trend Price: </p>
                         <p>{props.cardmarket.prices.trendPrice}</p>
                     </div>
                     <div className='row'>
-                        <p>Average 1</p>
+                        <p>Average 1: </p>
                         <p>{props.cardmarket.prices.avg1}</p>
                     </div>
                     <div className='row'>
-                        <p>Average 7</p>
+                        <p>Average 7: </p>
                         <p>{props.cardmarket.prices.avg7}</p>
                     </div>
                     <div className='row'>
-                        <p>Average 30</p>
+                        <p>Average 30: </p>
                         <p>{props.cardmarket.prices.avg30}</p>
                     </div>
                 </div>
