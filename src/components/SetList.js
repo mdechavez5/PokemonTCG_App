ff88a7b60d580d08131682560f31e46de8645cdb
mdechavez5 <marcdechavez@gmail.com>
2022-05-21 13:48:48 -0400

    Remove Set Link for now

--- src/components/SetList.js
+++ src/components/SetList.js
@@ -4,7 +4,6 @@ import {Link} from 'react-router-dom'
 function SetList() {
     return (
         <section className='set-list row'>
-            <Link to='/sets'>Sets</Link>
             <Link to='/sets/swsh9'>Brilliant Stars</Link>
             <Link to='/sets/swsh8'>Fusion Strike</Link>
             <Link to='/sets/swsh7'>Evolving Skies</Link>
